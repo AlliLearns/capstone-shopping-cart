a9ccf380d60aeed7532464a8b5d0367be9ed1097
AlliLearns <allilearns@gmail.com>
2024-02-10 06:47:25 -0600

    Successfully showing edit product form.

--- client/src/components/ListProducts.jsx
+++ client/src/components/ListProducts.jsx
@@ -1,26 +1,25 @@
 import { useState } from "react";
 
 export default function ListProducts({ onDelete, products }) {
-  const [showEdit, toggleShowEdit] = useState(false);
-
-  const handleToggleEdit = (event) => {
-    event.preventDefault();
-    toggleShowEdit((prevState) => !prevState);
-  };
 
   return (
     <div className="product-listing">
       <h2>Products</h2>
       <ul className="product-list">
-        {products.map(item => {
-          if (!showEdit) return <Product item={item} onDelete={onDelete} key={item._id}/>
-        })}
+        {products.map(item => <Product item={item} onDelete={onDelete} key={item._id}/>)}
       </ul>
     </div>
   );
 }
 
 function Product({ item, onDelete }) {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleShowEdit = (event) => {
+    event.preventDefault();
+    setShowEdit(true);
+  };
+
   const handleDelete = (event) => {
     console.log(item);
     event.preventDefault();
@@ -35,15 +34,54 @@ function Product({ item, onDelete }) {
         <p className="quantity">{item.quantity} left in stock</p>
         <div className="actions product-actions">
           <button className="add-to-cart">Add to Cart</button>
-          <button className="edit">Edit</button>
+          <button className="edit" onClick={handleShowEdit}>Edit</button>
+          {showEdit ? <EditProductForm /> : null}
         </div>
         <button className="delete-button" onClick={handleDelete}><span>X</span></button>
       </div>
     </li>);
 }
 
-function EditProductForm({ handleToggleEdit }) {
+function EditProductForm() {
   return (
-    <div className="edit-form"></div>
+    <div className="edit-form">
+    <h3>Edit Product</h3>
+    <form>
+      <div className="input-group">
+        <label htmlFor="product-name">Product Name</label>
+        <input
+          type="text"
+          id="product-name"
+          value="Apple 10.5-Inch iPad Pro"
+          aria-label="Product Name"
+        />
+      </div>
+
+      <div className="input-group">
+        <label htmlFor="product-price">Price</label>
+        <input
+          type="number"
+          id="product-price"
+          value="649.99"
+          aria-label="Product Price"
+        />
+      </div>
+
+      <div className="input-group">
+        <label htmlFor="product-quantity">Quantity</label>
+        <input
+          type="number"
+          id="product-quantity"
+          value="2"
+          aria-label="Product Quantity"
+        />
+      </div>
+
+      <div className="actions form-actions">
+        <button type="submit">Update</button>
+        <button type="button">Cancel</button>
+      </div>
+    </form>
+    </div>
   );
 }
